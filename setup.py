9ceb31877c45577b57c795accec62ea02226065c
Koushik Roy <92144539+koushik173@users.noreply.github.com>
2024-01-19 15:33:33 +0600

    error fixed

--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-'''def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements'''
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 REPO_NAME = "mongodb_connection"
-PKG_NAME= "MongoConnect"
+PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "koushik173"
 AUTHOR_EMAIL = "koushikroy298@gmail.com"
 
@@ -38,6 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
-    
-)
+    )
